a73e4dee7daf170646e14f5ac3de9fbc24579b66
Berta György <111363208+bertagyorgy@users.noreply.github.com>
2025-10-01 14:43:44 +0200

    Add files via upload

---
+++ src/main/java/com/example/myapplication2/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.myapplication2
+
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        var szam: Int = 0;
+        val gombElem = findViewById<Button>(R.id.gomb)
+        gombElem.setOnClickListener { szam++
+            gombElem.text = szam.toString()
+        }
+    }
+}
